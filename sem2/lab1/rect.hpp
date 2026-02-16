f6dae32b04ce7be8019831571f5a58a633ba98d8
Alexander Yurits <terruwork@gmail.com>
2026-02-16 18:27:09 +0300

    Code for 1.6. Added to the rect.hpp::rect printing out the: constructors purpose, constructors class address, values inside the constructors object, same with destructor. Wrote in comments where each constr/destr is called.

--- sem2/lab1/rect.hpp
+++ sem2/lab1/rect.hpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#define RED_TEXT "\033[31m"
+#define GREEN_TEXT "\033[32m"
+#define YELLOW_TEXT "\033[33m"
+#define COLOR_TEXT_FINISH "\033[0m"
+
 class Rect{
     public:
         int leftBorderX;
@@ -12,14 +17,23 @@ class Rect{
         rightBorderX = inpRightBorderX;
         upperBorderY = inpUpperBorderX;
         lowerBorderY = inpLowerBorderX;
+        std::cout << GREEN_TEXT << "Initialisation with given values constructor called. " << "Object address: " << this << " " << leftBorderX << " " << rightBorderX << " " << upperBorderY << " " << lowerBorderY << COLOR_TEXT_FINISH <<std::endl;
     }
     Rect(){
         leftBorderX = 0;
         rightBorderX = 0;
         upperBorderY = 0;
         lowerBorderY = 0;
+        std::cout << YELLOW_TEXT << "Default Initialisation Constructor called. " << "Object address: " << this << " " << leftBorderX << " " << rightBorderX << " " << upperBorderY << " " << lowerBorderY << COLOR_TEXT_FINISH << std::endl;
     }
     ~Rect(){
-        std::cout << leftBorderX << " " << rightBorderX << " " << upperBorderY << " " << lowerBorderY << std::endl;
+        std::cout << RED_TEXT << "Destructor called. " << "Object address: " << this << " " << leftBorderX << " " << rightBorderX << " " << upperBorderY << " " << lowerBorderY << COLOR_TEXT_FINISH << std::endl;
+    }
+    Rect(const Rect & other) {
+        leftBorderX = other.leftBorderX;
+        rightBorderX = other.rightBorderX;
+        upperBorderY = other.upperBorderY;
+        lowerBorderY = other.lowerBorderY;
+        std::cout << "Copying constructor called. Copying into " << this << " from " << &other << std::endl;
     }
 };
